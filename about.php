f7c79b55eb440f9fee8f72de77f897b79bce214f
sobhanaliyan <sobhanaliyan4@gmail.com>
2024-12-01 01:28:21 +0330

    v2

--- about.php
+++ about.php
@@ -2,10 +2,12 @@
 <?php
 include("head.html");
 ?>
+<div class="tecrar">
 <p>
      
 </p>
 <img src="">
+</div>
 <?php
 include("foot.html");
 ?>
--- call.php
+++ call.php
@@ -2,9 +2,15 @@
 <?php
 include("head.html");
 ?>
-<p>
-     
-</p>
+<div class="tecrar">
+<div>
+     <span>شماره تماس:43-42316640-031</span>
+ </div>
+ 
+ <div>
+    <span>ایتا / واتساپ:09134300158</span>
+ </div>
+</div>
 
 <?php
 include("foot.html");
